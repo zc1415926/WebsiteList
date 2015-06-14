870d0111e3a5f115148303507f7e90fc520bc1f5
zc1415926 <zc1415926@126.com>
2015-06-14 23:20:38 +0800

    setup basic layout and index page

---
+++ resources/views/layouts/partials/nav.blade.php
@@ -0,0 +1,24 @@
+<nav class="tm-navbar uk-navbar uk-navbar-attached">
+    <div class="uk-container uk-container-center">
+
+        <a class="uk-navbar-brand uk-hidden-small" href="index.html">
+            <img class="uk-margin uk-margin-remove" src="../images/logo_uikit.svg" width="90" height="30" title="UIkit" alt="UIkit">
+        </a>
+
+        <ul class="uk-navbar-nav uk-visible-large">
+            <li><a href="docs/documentation_get-started.html">Get Started</a></li>
+            <li><a href="docs/core.html">Core</a></li>
+        </ul>
+        <div class="uk-navbar-content uk-hidden-small uk-navbar-flip">
+            <form class="uk-form uk-margin-remove uk-display-inline-block">
+                <input type="text" placeholder="username">
+                <input type="password" placeholder="password">
+                <button class="uk-button uk-button-primary">Login</button>
+            </form>
+        </div>
+        <a href="#tm-offcanvas" class="uk-navbar-toggle uk-visible-small" data-uk-offcanvas></a>
+
+        <div class="uk-navbar-brand uk-navbar-center uk-visible-small"><img src="../images/logo_uikit.svg" width="90" height="30" title="UIkit" alt="UIkit"></div>
+
+    </div>
+</nav>
---
+++ resources/views/list/index.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.default')
+@section('content')
+    <h1>Hi, this is homepage! </h1>
+@stop
